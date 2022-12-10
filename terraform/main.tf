e396b296581e974bd06257ff3ffb6658b2bf0300
Ben Vaughan <ben@benvon.net>
2022-12-10 10:45:23 -0600

    functional vpc deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,30 +5,43 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    encrypt = true
+    bucket  = "dso-dojo-2022-12-bv-terraform-state"
+    key     = "terraform.tfstate"
+    dynamodb_table = "dso-dojo-2022-12-bv-terraform-state-lock"
+    region  = "us-west-1"
+  }
 }
 
 module "aws_vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-    name = "dso-dojo-2022-12-bv"
-    cidr = "10.99.0.0/16"
-    create_igw = true
-    azs = ["us-west-1a", "us-west-1b", "us-west-1c"]
-    public_subnets = ["10.99.66.0/24", "10.99.99.0/24", "10.99.33.0/24"]
-    private_subnets = ["10.99.11.0/24", "10.99.22.0/24", "10.99.44.0/24"]
-    enable_nat_gateway = true
+  name = "dso-dojo-2022-12-bv"
+  cidr = "10.99.0.0/16"
+  create_igw = true
+  azs = ["us-west-1a", "us-west-1b"]
+  public_subnets = ["10.99.66.0/24", "10.99.99.0/24"]
+  private_subnets = ["10.99.11.0/24", "10.99.22.0/24"]
+  enable_nat_gateway = false
   
 }
 
 resource "aws_s3_bucket" "remote_backend_storage" {
   bucket = "dso-dojo-2022-12-bv-terraform-state"
-  acl    = "private"
 }
+
+resource "aws_s3_bucket_acl" "remote_backend_storage_acl" {
+  bucket = aws_s3_bucket.remote_backend_storage.id
+  acl    = "private"  
+}
+  
+
 resource "aws_s3_bucket_versioning" "remote_state_versioning" {
   bucket = aws_s3_bucket.remote_backend_storage.id
   //enabled = true
   versioning_configuration {
-    
+    status = "Enabled"
   }
 }
 
